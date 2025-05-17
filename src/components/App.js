918ff588de42e5889ce72d7689f699c9f96a0a53
rambabu1458 <ramu.y95@gmail.com>
2025-05-17 11:18:19 +0530

    PageLayout done

--- src/components/App.js
+++ src/components/App.js
@@ -1,19 +1,19 @@
-
 import React from "react";
-import './../styles/App.css';
+import "./../styles/App.css";
 import PageLayout from "./PageLayout";
 
 const App = () => {
   return (
     <div>
-        {/* Do not remove the main div */}
-        <PageLayout 
+      {/* Do not remove the main div */}
+      <PageLayout
         header={<h1>Welcome to my website</h1>}
         footer={<p>&copy; 2023 My Website.All rights reserved.</p>}
       >
-        <p>This is the content of my website.</p></PageLayout>
+        <p>This is the content of my website.</p>
+      </PageLayout>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
--- src/components/PageLayout.jsx
+++ src/components/PageLayout.jsx
@@ -1,13 +1,13 @@
-import React from 'react'
+import React from "react";
 
-const PageLayout = ({header,children,footer}) => {
+const PageLayout = ({ header, children, footer }) => {
   return (
     <div>
-        <header>{header}</header>
-        <main>{children}</main>
-        <footer>{footer}</footer>
+      <header>{header}</header>
+      <main>{children}</main>
+      <footer>{footer}</footer>
     </div>
-  )
-}
+  );
+};
 
-export default PageLayout
+export default PageLayout;
